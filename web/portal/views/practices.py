460fdcf2fda3795674b85e072190668e821407de
Richard Bramley <rabramley@gmail.com>
2016-10-01 14:28:26 +0100

    Page and search registrations

--- web/portal/views/practices.py
+++ web/portal/views/practices.py
@@ -3,10 +3,25 @@ from portal import app, db
 from portal.models import *
 from portal.forms import *
 
-@app.route('/practices', methods=['GET'])
-def practices_index():
-    practices = PracticeRegistration.query.order_by(PracticeRegistration.date_created.desc()).all()
-    return render_template('practices/index.html', practices=practices)
+@app.route('/practices/')
+@app.route('/practices/<int:page>')
+def practices_index(page=1):
+    searchString = request.args.get('search')
+    searchForm = SearchForm(search=searchString)
+
+    q = PracticeRegistration.query.join(Practice, PracticeRegistration.practice)
+
+    if searchString:
+        q = q.filter(Practice.name.like("%{0}%".format(searchString)))
+
+    registrations = (
+        q.order_by(Practice.name.asc())
+         .paginate(
+            page=page,
+            per_page=10,
+            error_out=False))
+
+    return render_template('practices/index.html', registrations=registrations, searchForm=searchForm)
 
 @app.route('/practices/add/list/')
 @app.route("/practices/add/list/<int:page>")
@@ -15,7 +30,8 @@ def practices_add_list(page=1):
     searchForm = SearchForm(search=searchString)
     selectForm = PracticeRegisterForm()
 
-    q = Practice.query
+    q = Practice.query.filter(
+        ~Practice.code.in_(db.session.query(PracticeRegistration.code)))
 
     if searchString:
         q = q.filter(Practice.name.like("%{0}%".format(searchString)))
