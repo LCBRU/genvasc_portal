a78109332d7e54a8e6156b1008ed3ab88826c878
Richard Bramley <rabramley@gmail.com>
2016-10-03 08:16:45 +0100

    Button styling

--- web/portal/views/home.py
+++ web/portal/views/home.py
@@ -4,13 +4,6 @@ from portal import app, db
 
 from portal.models import *
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        name = request.form['name']
-        code = request.form['code']
-        practice = Practice(name, code)
-        db.session.add(practice)
-        db.session.commit()
-    practices = Practice.query.order_by(Practice.date_created.desc()).all()
-    return render_template('index.html', practices=practices)
+    return render_template('index.html')
