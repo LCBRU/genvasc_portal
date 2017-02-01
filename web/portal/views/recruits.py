c332dcb6ecf48058e000373270f52a70f48ddfc2
Richard Bramley <rabramley@gmail.com>
2017-02-01 12:59:39 +0000

    Search against name and study id

--- web/portal/views/recruits.py
+++ web/portal/views/recruits.py
@@ -6,6 +6,7 @@ from portal.models import *
 from portal.forms import *
 from portal.helpers import *
 from portal.datatypes import *
+from sqlalchemy import or_, func
 
 @app.route('/practices/<string:code>/recruits')
 @app.route('/practices/<string:code>')
@@ -19,8 +20,13 @@ def recruits_index(code):
     q = Recruit.query.join(PracticeRegistration, Recruit.practice_registration).filter(PracticeRegistration.code == code)
 
     if searchForm.search.data:
-        # TODO
-        q = q.filter(Recruit.nhs_number.like("%{}%".format(searchForm.search.data)))
+        string_in_name = db.session.query(RecruitStatus.id).filter(func.concat(RecruitStatus.first_name, ' ', RecruitStatus.last_name).ilike("%{}%".format(searchForm.search.data)))
+        string_in_study_id = db.session.query(RecruitStatus.id).filter(RecruitStatus.study_id.ilike("%{}%".format(searchForm.search.data)))
+        q = q.filter(or_(
+            Recruit.nhs_number.like("%{}%".format(searchForm.search.data)),
+            Recruit.id.in_(string_in_name),
+            Recruit.id.in_(string_in_study_id)
+            ))
 
     recruits = (
         q.order_by(Recruit.date_recruited.desc())
@@ -30,6 +36,7 @@ def recruits_index(code):
             error_out=False))
 
     return render_template('practices/recruits/index.html', recruits=recruits, practice_registration=practice_registration, searchForm=searchForm)
+
 
 @app.route('/practices/<string:code>/recruits/add', methods=['GET','POST'])
 @must_exist(model=PracticeRegistration, field=PracticeRegistration.code, request_field='code', error_redirect='practices_index', message="Practice is not registered")
