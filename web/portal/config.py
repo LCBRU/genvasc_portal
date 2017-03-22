e277ebbe6948e5456b2d8c2534be64a33d1cb7d7
Richard Bramley <rabramley@gmail.com>
2017-03-22 14:00:22 +0000

    Address SMTOP settings

--- web/portal/config.py
+++ web/portal/config.py
@@ -2,7 +2,7 @@ import os
 
 class BaseConfig(object):
     SECRET_KEY = os.environ['GGPP_FLASK_SECRET_KEY']
-    DEBUG = os.environ['GGPP_FLASK_DEBUG']
+    DEBUG = os.environ['GGPP_FLASK_DEBUG'] == 'True'
     DB_HOST = os.environ['GGPP_DB_HOST']
     DB_NAME = os.environ['GGPP_DB_NAME']
     DB_USER = os.environ['GGPP_DB_USER']
@@ -10,8 +10,12 @@ class BaseConfig(object):
     SQLALCHEMY_DATABASE_URI = 'mysql://{0}:{1}@{2}/{3}'.format(
         DB_USER, DB_PASS, DB_HOST, DB_NAME
     )
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ['GGPP_SQLALCHEMY_TRACK_MODIFICATIONS']
-    SQLALCHEMY_ECHO=os.environ['GGPP_SQLALCHEMY_ECHO']
+    SQLALCHEMY_TRACK_MODIFICATIONS=os.environ['GGPP_SQLALCHEMY_TRACK_MODIFICATIONS'] == 'True'
+    SQLALCHEMY_ECHO=os.environ['GGPP_SQLALCHEMY_ECHO'] == 'True'
     SECURITY_PASSWORD_HASH=os.environ['GGPP_SECURITY_PASSWORD_HASH']
     SECURITY_PASSWORD_SALT=os.environ['GGPP_SECURITY_PASSWORD_SALT']
-    SECURITY_TRACKABLE=os.environ['GGPP_SECURITY_TRACKABLE']
+    SECURITY_TRACKABLE=os.environ['GGPP_SECURITY_TRACKABLE'] == 'True'
+    SMTP_SERVER = 'localhost'
+    APPLICATION_EMAIL_ADDRESS = os.environ['LCBRUIT_EMAIL_ADDRESS']
+    ADMIN_EMAIL_ADDRESSES = os.environ['ADMIN_EMAIL_ADDRESS']
+    ERROR_EMAIL_SUBJECT = 'GENVASC Portal Error'
