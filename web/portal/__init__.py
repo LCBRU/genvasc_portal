8ec40d92e804952dfdfd29ec8e90d043825a0c9f
Richard Bramley <rabramley@gmail.com>
2017-05-26 11:00:30 +0100

    Add password policy

--- web/portal/__init__.py
+++ web/portal/__init__.py
@@ -6,45 +6,7 @@ from flask_security import Security, SQLAlchemyUserDatastore
 import logging
 import traceback
 from flask_mail import Mail
-
-
-class ReverseProxied(object):
-    '''Wrap the application in this middleware and configure the
-    front-end server to add these headers, to let you quietly bind
-    this to a URL other than / and to an HTTP scheme that is
-    different than what is used locally.
-
-    In nginx:
-    location /myprefix {
-        proxy_pass http://192.168.0.1:5001;
-        proxy_set_header Host $host;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Scheme $scheme;
-        proxy_set_header X-Script-Name /myprefix;
-        }
-
-    :param app: the WSGI application
-    '''
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
-        if script_name:
-            environ['SCRIPT_NAME'] = script_name
-            path_info = environ['PATH_INFO']
-            if path_info.startswith(script_name):
-                environ['PATH_INFO'] = path_info[len(script_name):]
-
-        scheme = environ.get('HTTP_X_SCHEME', '')
-        if scheme:
-            environ['wsgi.url_scheme'] = scheme
-
-        server = environ.get('HTTP_X_FORWARDED_SERVER', '')
-        if server:
-            environ['HTTP_HOST'] = server
-
-        return self.app(environ, start_response)
+from portal.utils import ResetPasswordForm, ChangePasswordForm, ReverseProxied
 
 
 app = Flask(__name__)
@@ -82,7 +44,9 @@ database.init_db()
 from portal.models import *
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security(app, user_datastore)
+security = Security(app, user_datastore,
+                    reset_password_form=ResetPasswordForm,
+                    change_password_form=ChangePasswordForm)
 
 from portal.views import *
 import portal.security
--- web/portal/security.py
+++ web/portal/security.py
@@ -1,31 +1,36 @@
 import os
 import yaml
+import string
+import random
 from portal import app, db, user_datastore
 from portal.models import *
-from flask_security import Security, SQLAlchemyUserDatastore
 
-user_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'users.yml')
+user_file_path = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    'users.yml')
+
 
 @app.before_first_request
 def init_security():
-    admin_role = user_datastore.find_or_create_role(name=Role.ADMIN_ROLENAME, description='Administration')
-    
+    admin_role = user_datastore.find_or_create_role(
+        name=Role.ADMIN_ROLENAME,
+        description='Administration')
+
     user_file = open(user_file_path, "r")
     users = yaml.load_all(user_file)
     for user_details in users:
 
         user = user_datastore.get_user(user_details['email'])
         if not user:
-            print('*' * 20)
-            print(f"Creating User: {user_details['email']}")
-            print('*' * 20)
+            app.logger.info("Creating User: {}".format(user_details['email']))
+            app.logger.info("Password: {}".format(get_random_password()))
 
             user = user_datastore.create_user(
                 email=user_details['email'],
-                password=user_details['password'],
+                password=get_random_password(),
                 first_name=user_details['first_name'],
                 last_name=user_details['last_name']
-                )
+            )
 
         if not user_details.get('inactive', False):
             user_datastore.activate_user(user)
@@ -39,9 +44,22 @@ def init_security():
         if "practices" in user_details:
 
             for practice_code in user_details['practices']:
-                practice = PracticeRegistration.query.filter(PracticeRegistration.code == practice_code).first()
+                practice = PracticeRegistration.query.filter(
+                    PracticeRegistration.code == practice_code
+                ).first()
 
                 if practice:
                     user.practices.append(practice)
 
         db.session.commit()
+
+
+def get_random_password():
+
+    password_chars = random.sample(string.ascii_lowercase, 10)
+    password_chars += random.sample(string.ascii_uppercase, 10)
+    password_chars += random.sample(string.punctuation, 10)
+
+    random.shuffle(password_chars)
+
+    return "".join(password_chars)
